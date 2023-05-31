e039d7b6667e6451bc85a75c753fce2e4717ef26
Max Chechel <maksymchechel@yahoo.com>
2023-05-31 14:12:14 +0200

    Added src folder and config to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
+    plugins: [eslintPlugin({ cache: false })],
+    server: {
+        port: 3000,
+    },
+    build: {
+        minify: true,
+        manifest: true,
+        rollupOptions: {
+            input: './src/main.js',
+        },
+    },
 });
